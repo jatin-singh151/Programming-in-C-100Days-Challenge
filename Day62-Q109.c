e7baa23b48df2538f880787fb487a8e13c8eeecf
jatin-singh151 <Jatin.28117@stu.upes.ac.in>
2025-12-01 09:53:49 +0530

    Update Day62-Q109.c

--- Day62-Q109.c
+++ Day62-Q109.c
@@ -1,4 +1,4 @@
-Q113: Write a program to take an integer array arr and an integer k as inputs. The task is to find the kth smallest element in the array. Print the kth smallest element as output.
+Q109: Write a program to take an integer array arr and an integer k as inputs. The task is to find the kth smallest element in the array. Print the kth smallest element as output.
 
 /*
 Sample Test Cases:
