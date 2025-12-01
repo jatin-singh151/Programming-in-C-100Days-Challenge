2bb629e6a9a9531513aabd9f1fd4c09d4139cfe1
jatin-singh151 <Jatin.28117@stu.upes.ac.in>
2025-12-01 09:54:03 +0530

    Update Day63-Q110.c

--- Day63-Q110.c
+++ Day63-Q110.c
@@ -1,4 +1,4 @@
-Q114: Write a program to take a string s as input. The task is to find the length of the longest substring without repeating characters. Print the length as output.
+Q110: Write a program to take a string s as input. The task is to find the length of the longest substring without repeating characters. Print the length as output.
 
 /*
 Sample Test Cases:
