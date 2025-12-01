4eaeb657ffb1c0d3bccfac76fd01eed7aaad32b5
jatin-singh151 <Jatin.28117@stu.upes.ac.in>
2025-12-01 09:55:14 +0530

    Update Day66-Q113.c

--- Day66-Q113.c
+++ Day66-Q113.c
@@ -1,4 +1,4 @@
-Q115: Write a program to take two strings s and t as inputs (assume all characters are lowercase). The task is to determine if s and t are valid anagrams, meaning they contain the same characters with the same frequencies. Print "Anagram" if they are, otherwise "Not Anagram".
+Q113: Write a program to take two strings s and t as inputs (assume all characters are lowercase). The task is to determine if s and t are valid anagrams, meaning they contain the same characters with the same frequencies. Print "Anagram" if they are, otherwise "Not Anagram".
 
 /*
 Sample Test Cases:
