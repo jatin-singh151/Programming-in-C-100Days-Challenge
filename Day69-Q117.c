dec80db15fdd483789bff479a36b934782d96d62
jatin-singh151 <Jatin.28117@stu.upes.ac.in>
2025-12-01 09:56:12 +0530

    Update Day69-Q117.c

--- Day69-Q117.c
+++ Day69-Q117.c
@@ -1,4 +1,4 @@
-Q119: Write a program to take an integer array as input. Only one element will be repeated. Print the repeated element. Try to find the result in one single iteration.
+Q117: Write a program to take an integer array as input. Only one element will be repeated. Print the repeated element. Try to find the result in one single iteration.
 
 /*
 Sample Test Cases:
